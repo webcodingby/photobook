d1c45aa1870da65e3373d3affa53410f5d7e61f8
WCB <webcodingby@yandex.by>
2021-02-02 00:43:10 +0300

    add test.html

--- src/js/modules/login.js
+++ src/js/modules/login.js
@@ -3,7 +3,6 @@ const login = () => {
     const trigger = document.querySelectorAll(triggerSelector),
       modal = document.querySelector(modalSelector),
       close = document.querySelector(closeSelector);
-
   }
 
   
